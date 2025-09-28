f788cb954d445c1a6224f47bd07c14e0953c6f2b
Yoel Leghesse <yoelleghesse@gmail.com>
2025-09-28 17:04:45 -0400

    added update display func for nums, operations, and dot. Also added clear

--- main.js
+++ main.js
@@ -25,7 +25,6 @@ const divide = function(a, b) {
 
 
 // operator + function to determine which function I want calculator to do
-
 function operatorFunc(op, a, b) {
     switch (op) {
         case "+":
@@ -40,4 +39,42 @@ function operatorFunc(op, a, b) {
         case "/":
             divide(a, b)
     }
+}
+
+// displaying the num in display
+function updateDisplay(num) {
+    const display = document.querySelector(".display")
+    display.textContent = num
+}
+
+// adding event listeners to each number and dot
+const numbers = document.querySelectorAll('.number');
+for (const num of numbers) {
+    num.addEventListener("click", () => {
+        updateDisplay(num.textContent)
+    })
+}
+
+const dot = document.querySelector('.dot');
+dot.addEventListener('click', () => {
+    updateDisplay(dot.textContent)
+})
+
+// clearing the screen
+function clearDisplay() {
+    const display = document.querySelector(".display")
+    display.textContent = ""
+}
+const clear = document.querySelector('.clear')
+clear.addEventListener('click', () => {
+    clearDisplay()
+})
+
+
+// adding operations to the screen
+const operations = document.querySelectorAll(".operation");
+for (const op of operations) {
+    op.addEventListener("click", () =>  {
+        updateDisplay(op.textContent)
+    })
 }
